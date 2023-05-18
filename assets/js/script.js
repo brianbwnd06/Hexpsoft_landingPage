24591d07d42eb3264ae2e0a099f1522d3a404081
brianbwnd06 <bwndbrayen06@gmail.com>
2023-05-18 18:40:33 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active', window.scrollY>0);
+});
+
+const btnMode = document.getElementById('btn-mode');
+const iconMode = document.getElementById('icon-mode');
+const body = document.querySelector('body');
+
+btnMode.addEventListener('click', function(){
+    body.classList.toggle('dark');
+    iconMode.classList.toggle('fa-moon')
+    iconMode.classList.toggle('fa-sun')
+})
+
+const btnModeMobile = document.getElementById('btn-mode-mobile');
+const bodyMobile = document.querySelector('body');
+const navMenu = document.getElementById('nav-menu');
+const hamMenu = document.getElementById('hamMenu');
+const menuLink = document.querySelectorAll('.nav-menu li');
+
+btnModeMobile.addEventListener('click', function(){
+    this.classList.toggle('active')
+    bodyMobile.classList.toggle('dark');
+    navMenu.classList.remove("active");
+    hamMenu.classList.remove("active");
+})
+
+hamMenu.addEventListener('click', function(){
+    this.classList.toggle('active')
+    navMenu.classList.toggle('active');
+})
+
+for (var i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener("click", function() {
+        navMenu.classList.remove("active");
+        hamMenu.classList.remove("active");
+    });
+  }
